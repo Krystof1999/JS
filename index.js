46e9bb535343aa4bffcf7fd11f9bf2150ce1dfc6
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2022-11-05 11:06:50 +0100

    Commit 5.11. sourcethree

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ document.getElementById('p3').innerHTML = 'Hello p3'
 // let username = window.prompt('Hello how are you?')
 // console.log(username)
 
-
 // let username;
 
 // document.getElementById('myButton').onclick = function() {
@@ -22,10 +21,46 @@ document.getElementById('p3').innerHTML = 'Hello p3'
 
 // TYPE CONVERTION
 
-let age = window.prompt('how old are you')
+// let age = window.prompt('how old are you')
 
-console.log(typeof age)
-age = Number(age)
-age += 1;
-console.log(typeof age)
-console.log('HP', age, "years")
+// console.log(typeof age)
+// age = Number(age)
+// age += 1;
+// console.log(typeof age)
+// console.log('HP', age, "years")
+
+// MATH
+
+// let x = 3.14;
+
+// x = Math.round(x);
+
+// CHECKBOX PROPERTY
+
+{
+  /* <label for="visa">Visa</label>
+<input type="radio" name="card" id="visa" />
+<label for="masterCard">masterCard</label>
+<input type="radio" name="card" id="masterCard" />
+<label for="applePay">applePay</label>
+<input type="radio" name="card" id="applePay" /> */
+}
+
+// const visa = document.getElementById("visa");
+// const masterCard = document.getElementById("masterCard");
+// const applePay = document.getElementById("applePay");
+// const submitBtn = document.getElementById("btn");
+
+// submitBtn.onclick = function () {
+//   if (visa.checked) {
+//     console.log("Visa payment");
+//   } else if (masterCard.checked) {
+//     console.log("Mastercard payment");
+//   } else if (applePay.checked) {
+//     console.log("Applepay payment");
+//   } else {
+//     console.log("Please select payment method");
+//   }
+// };
+
+// CONTINUE 1:23
