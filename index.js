4dcd0b5f9ecc5392c0aec848c747e420ff9a6aa2
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2022-11-11 16:15:25 +0100

    no message

--- index.js
+++ index.js
@@ -102,46 +102,173 @@ document.getElementById('p3').innerHTML = 'Hello p3'
 // }
 
 // TEMPERATURE CONVERTION
-function toCelsius(temperature){
-return (temperature - 32) * (5/9)
-}
+// function toCelsius(temperature){
+// return (temperature - 32) * (5/9)
+// }
 
-function toFahrenheit(temperature){
-return temperature * 9 / 5 + 32
-}
+// function toFahrenheit(temperature){
+// return temperature * 9 / 5 + 32
+// }
 
-let temperature = document.getElementById('textBox')
+// let temperature = document.getElementById('textBox')
 
-const submitBtn = document.getElementById('submitButton')
+// const submitBtn = document.getElementById('submitButton')
 
-const toCesiusRadio = document.getElementById('cButton')
+// const toCesiusRadio = document.getElementById('cButton')
 
-const toFahrenheitRadio = document.getElementById('fButton')
+// const toFahrenheitRadio = document.getElementById('fButton')
 
-submitBtn.onclick = function() {
- temperature = temperature.value
+// let resultLabel = document.getElementById('temperatureLabel')
 
- if(toCesiusRadio.checked){
-    temperature = toCelsius(temperature)
-    console.log(temperature)
- }
- else if(toFahrenheitRadio.checked){
-    temperature = toFahrenheit(temperature)
-    console.log(temperature)
- }
- else{
-    alert('Please choose one of the options.')
- }
+// submitBtn.onclick = function() {
+//  if(toCesiusRadio.checked){
+//    temperature = temperature.value
+//    temperature = Number(temperature)
+//     temperature = toCelsius(temperature)
+//     console.log(temperature)
+//     resultLabel.innerHTML = temperature
+//  }
+//  else if(toFahrenheitRadio.checked){
+//    temperature = temperature.value
+//    temperature = Number(temperature)
+//     temperature = toFahrenheit(temperature)
+//     console.log(temperature)
+//     resultLabel.innerHTML = temperature
+//  }
+//  else{
+//     resultLabel.innerHTML = 'Select a unit'
+//  }
+
+// }
+
+// SPREAD OPERATOR ...
+// z array dostanu hotony jedotlive
+// ze stringu dostanu pismena jednotlive
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7]
+
+// console.log(...numbers)
+
+// REST PARAMETERS ...
+// do funkce nedam parametry number1 number2. Vezme to vsechny parametry
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 5;
+
+// console.log(sum(a,b,c,d,e,f))
+
+// function sum(...numbers){
+// let total = 0;
+
+// for(let number of numbers){
+//    total += number
+// }
+
+// return total;
+// }
+
+// CALLBACK FUNCTION
+
+// // sum(2, 3, something)
+// sum(2, 5, displayConsole); // udela sum 2 a 5 a az potom udela displayConsole funkci
+
+// function sum(x, y, callBack){
+//    let resutl = x + y;
+//    callBack(resutl) // to se pak dosadi do displayConsole(sem)
+// }
+
+// function displayConsole(output){
+//     console.log(output)
+// }
+
+// array.forEach(element => {
+   
+// });
+
+// // FOR EACH
+
+// let students = ['student1', 'student2', 'student3']
+
+// // students.forEach((student, idx) => {
+// //    console.log(student + idx)
+// // })
+
+// students.forEach(displayStudents)
+
+// function displayStudents (student, idx) {
+// console.log(student + "student index: " + idx)
+// }
+
+// ARRAY MAP
+// pro kazdy element v array udela callback funkci a vytvori novy array
+
+// let numbers = [1, 2, 3, 4, 5]
+// let squares = numbers.map(square);
+
+// // console.log(square(numbers)) // not working
+
+// squares.forEach(print)
+
+// function square(element){
+//    return Math.pow(element, 2);
+// }
+
+// function print(element){
+//    console.log(element)
+// }
 
 
-}
+// ARRAY FILTER METHOD
+
+// let ages = [11, 15, 16, 17, 20, 21 , 35, 89]
+// let adults = ages.filter(checkAge)
 
 
+// adults.forEach(print)
+
+// function checkAge(element){
+//    return element > 18
+// }
+
+// function print(element){
+//    console.log(element)
+// }
+
+// ARRAY REDUCE
+// z array udela jednu hodnotu
+
+// let prices = [11, 10, 15, 20]
+// let total = prices.reduce(totalPrice)
+
+// console.log(total)
+
+// function totalPrice(total, element){
+//    return total + element;
+// }
 
 
+// SORTING NUMBERS IN ARRAY
+
+// let grades = [90, 10, 50, 30, 70]
+// grades = grades.sort(descendingSort)
+
+// grades.forEach(print)
 
 
+// function descendingSort(x, y){
+//    return y - x;
+// }
 
+// function ascendingSort(x, y){
+//    return x - y;
+// }
 
+// function print(element){
+//    console.log(element)
+// }
 
-
+// 45 continue
