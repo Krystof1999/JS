ce6a8177e03988df7c130f8b5a6714c0788e03df
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2022-11-16 18:09:59 +0100

    prague 16.11.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const date = new Date();
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
 
-const x = date.getTime();
+  if (hh == 0) {
+    hh = 12;
+  }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
 
-var countDownDate = new Date("2017-11-17T21:30:30Z").getTime();
-var distance = countDownDate - x;
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
+  ss = ss < 10 ? "0" + ss : ss;
 
-// const hours = date.getHours();
-// const minutes = date.getMinutes();
-// const seconds = date.getSeconds();
+  let time = hh + ":" + mm + ":" + ss + " " + session;
 
-console.log(date);
-console.log(x);
-console.log(countDownDate);
-console.log(distance);
-// console.log(hours);
-// console.log(minutes);
-// console.log(seconds);
-
-// document.getElementById("hoursLabel").innerHTML = hours + ":";
-// document.getElementById("minutesLabel").innerHTML = minutes + ":";
-// document.getElementById("secondsLabel").innerHTML = seconds;
-
-// var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-// var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-// var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("clock").innerText = time;
+  let t = setTimeout(function () {
+    currentTime();
+  }, 1000);
+}
+currentTime();
