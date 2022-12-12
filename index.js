1272855e3f72e30c3d7e93f94e6cdfbd2eebff26
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2022-12-12 07:44:53 +0100

    prg 12.12.

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-function currentTime() {
+// function currentTime() {
+//   let date = new Date();
+//   let hh = date.getHours();
+//   let mm = date.getMinutes();
+//   let ss = date.getSeconds();
+//   let session = "AM";
+
+//   if (hh == 0) {
+//     hh = 12;
+//   }
+//   if (hh > 12) {
+//     hh = hh - 12;
+//     session = "PM";
+//   }
+
+//   hh = hh < 10 ? "0" + hh : hh;
+//   mm = mm < 10 ? "0" + mm : mm;
+//   ss = ss < 10 ? "0" + ss : ss;
+
+//   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+//   document.getElementById("clock").innerText = time;
+//   let t = setTimeout(function () {
+//     currentTime();
+//   }, 1000);
+// }
+// currentTime();
+
+function current() {
   let date = new Date();
-  let hh = date.getHours();
-  let mm = date.getMinutes();
-  let ss = date.getSeconds();
-  let session = "AM";
+  let seconds = date.getSeconds();
 
-  if (hh == 0) {
-    hh = 12;
-  }
-  if (hh > 12) {
-    hh = hh - 12;
-    session = "PM";
-  }
+  document.getElementById("time").innerHTML = seconds;
 
-  hh = hh < 10 ? "0" + hh : hh;
-  mm = mm < 10 ? "0" + mm : mm;
-  ss = ss < 10 ? "0" + ss : ss;
+  setTimeout(seconds, 1000);
 
-  let time = hh + ":" + mm + ":" + ss + " " + session;
-
-  document.getElementById("clock").innerText = time;
   let t = setTimeout(function () {
-    currentTime();
+    current();
   }, 1000);
 }
-currentTime();
+current();
