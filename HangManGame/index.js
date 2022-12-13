10c86cf9cd62083a6ca46f6898a50068550930e4
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2022-12-13 22:00:26 +0100

    prg 13.12.

--- HangManGame/index.js
+++ HangManGame/index.js
@@ -1,8 +1,12 @@
 // const alphabetBtn = document.querySelectorAll("#alphabetButtons");
 const alphabetBtns = document.querySelector("#alphabetButtons");
+const word = document.getElementById("wordDiv");
+import { alphabet } from "./alphabet.js";
 
-const alphabet = ["A", "B", "C", "D", "E"];
+const finalWord = ["P", "R", "A", "G", "U", "E"];
+let sameLetter;
 
+// buttons creation
 alphabet.forEach((alpha) => {
   var alphaBtn = document.createElement("button");
   alphaBtn.id = "alphaBtn";
@@ -11,6 +15,31 @@ alphabet.forEach((alpha) => {
   alphabetBtns.appendChild(alphaBtn);
 
   alphaBtn.onclick = function () {
-    console.log(alpha);
+    // console.log(alpha); // alpha je letter
+
+    finalWord.forEach((letter) => {
+      if (letter == alpha) {
+        sameLetter = letter;
+
+        console.log(sameLetter);
+      }
+    });
   };
+});
+
+finalWord.forEach((finalLetter) => {
+  var letterDiv = document.createElement("div");
+  letterDiv.id = "letterDiv";
+  var letters = document.createElement("p");
+  letters.id = "letter";
+  letters.innerHTML = finalLetter;
+
+  letterDiv.style.borderBottom = "2px solid black";
+  letterDiv.style.float = "left";
+  letterDiv.style.margin = "40px";
+
+  // letters.style.visibility = "hidden";
+
+  letterDiv.append(letters);
+  word.appendChild(letterDiv);
 });
