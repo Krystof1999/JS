7b6115fdfb813ba56a5209351e86d4da562bc2dc
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2022-12-18 14:17:50 +0100

    Hangman game withhout canvas finished, test folder added

--- HanngManOrWomanGame/playTheGame.js
+++ HanngManOrWomanGame/playTheGame.js
@@ -8,6 +8,7 @@ export function playTheGame() {
   let btnLetterHtml;
   var letterArray = [];
   var count = 0;
+  var lives = 10;
 
   word.forEach((w) => {
     var letterDiv = document.createElement("div");
@@ -25,7 +26,7 @@ export function playTheGame() {
   buttons.forEach((btn) => {
     btn.onclick = function () {
       var correct = false;
-      count += 1;
+      // count += 1;
       btnLetterHtml = btn.innerHTML; //! btn letter
       letterArray.forEach((letter) => {
         finalLetter = letter.innerHTML; //! P R A G
@@ -33,14 +34,44 @@ export function playTheGame() {
           letter.style.visibility = "visible";
           correct = true;
           correctCount += 1;
+          // setTimeout(btnDesibled(btn), 1000);
+          console.log(`btnLetterHtml ${btnLetterHtml}`);
+          console.log(`finalLetter ${finalLetter}`);
+          // console.log(`correctCount ${correctCount}`);
         }
       });
-      if (count == 10) {
-        alert("Game over");
-      }
       if (correctCount == wordLength) {
-        alert("You win");
+        setTimeout(youWin, 200);
+      } else if (count == 9) {
+        setTimeout(youLose(word), 200);
       }
+      if (correct != true) {
+        lives -= 1;
+        count += 1;
+      }
+      setTimeout(btnDesibled(btn), 1000);
+      // lives -= 1;
+      livesLeft(lives);
+      console.log(`correctCount ${correctCount}`);
+      console.log(`lives ${lives}`);
+      console.log(`count ${count}`);
     };
   });
+}
+
+function youWin() {
+  alert("You win");
+}
+function youLose(word) {
+  alert(`Game over. The word you were looking for was ${word}`);
+}
+
+function livesLeft(livesLeft) {
+  const lives = document.getElementById("lives");
+
+  lives.innerHTML = `You have ${livesLeft} lives`;
+}
+
+function btnDesibled(btn) {
+  btn.disabled = true;
 }
