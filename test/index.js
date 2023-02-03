863190b3f195a19758803007ec3727dbbfef41f6
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-02-03 11:11:59 +0100

    update

--- test/index.js
+++ test/index.js
@@ -1,1 +1,15 @@
+const array = [
+  { id: 1, name: "adam" },
+  { id: 2, name: "petr" },
+];
 
+const arr = Array(array);
+console.log(arr);
+
+const mapArr = arr.map((ar) => JSON.stringify(ar));
+
+console.log(mapArr[0]);
+
+// for (let i = 0; i < mapArr.length; i++) {
+//   console.log(mapArr[i]);
+// }
