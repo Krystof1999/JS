4ad3e470aa4d5ad54f5b81c9daf9dd4f15d9fac0
Krystof Bumbalek <krystof.bumbalek@saboit.de>
2023-02-28 21:04:20 +0100

    update

--- api-cookbook/app.js
+++ api-cookbook/app.js
@@ -3,9 +3,9 @@ const httpFetch = new Fetch();
 // Init UI
 const ui = new UI();
 
-// search input
 const searchField = document.querySelector(".foodSearch");
 const searchButton = document.querySelector(".searchButton");
+const foodImage = document.querySelector(".food-image");
 
 let inputValue;
 
@@ -15,14 +15,12 @@ searchButton.addEventListener("click", () => {
   if (inputValue !== "") {
     httpFetch.getFood(inputValue).then((data) => {
       if (data.results.length > 0) {
-        console.log(data);
-
         ui.displayFood(data);
       } else {
         // Display message: "No food found, please try it again."
         console.log("zero");
       }
-    }); // url + pasta...
+    });
   } else {
     // Display message: "Please enter food you want to search for"
     console.log("empty");
